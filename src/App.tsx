eeaa05be88bb043be687eb3b0dfaea64403c4d49
hardeepbharaj <bharajhardeep23@gmail.com>
2024-06-17 16:37:57 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import Canvas from './Canvas';
+import Header from './Header';
+import Cursor from './components/svg/ColorPicker.svg';
+
+import './App.scss';
+
+function App() {
+  const [dropperCursor, setDropperCursor] = useState<boolean>(false);
+  const [pickedColor, setPickedColor] = useState<string>('');
+
+  return (
+    <div className="App" style={{ cursor: dropperCursor ? `url('${Cursor}'), auto` : 'default' }}>
+      <Header
+        dropperCursor={dropperCursor}
+        pickedColor={pickedColor}
+        setDropperCursor={setDropperCursor}
+      />
+      <Canvas
+        dropperCursor={dropperCursor}
+        setPickedColor={setPickedColor}
+      />
+    </div>
+  );
+}
+
+export default App;
